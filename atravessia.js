624ac79489528a238e456ec604c5a7fce6c0b9c9
João Marcos Aires Duarte <joao.duarte@ITBAM.ORG.BR>
2022-01-05 16:19:24 -0400

    Refactory do Arquivo atravessia.js

---
+++ atravessia.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(500, 400);
+  }
+  
+  function draw() {
+    background(imagemDaEstrada);
+    mostraAtor();
+    mostraCarro();
+    movimentaCarro();
+    movimentaAtor1();
+  }
