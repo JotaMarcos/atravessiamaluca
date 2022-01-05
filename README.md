07e3edd3bcf2cfef3331de8a8c6046a35e424e63
João Marcos Aires Duarte <joao.duarte@ITBAM.ORG.BR>
2022-01-05 16:00:33 -0400

    Criação do Jogo Atravessia Maluca

---
+++ README.md
@@ -0,0 +1,7 @@
+##### ################### Jogo de Clássico de Atravessia Maluca ###################
+
+- Criar um jogo do zero, com animações, pontuação e sons.
+- Aprender adicionar imagens no P5 Js.
+- Aprender na prática como usar listas (arrays).
+- Entender como evitar código duplicado com laços de repetição.
+- Desenvolver e aprimorar a Lógica de Programa em JavaScript.
---
+++ imagens.js
@@ -0,0 +1,14 @@
+let imagemDaEstrada;
+let imagemDoAtor1;
+let imagemCarro1;
+let imagemCarro2;
+let imagemCarro3;
+
+function preload(){
+    imagemDaEstrada = loadImage("imagens/estrada.png");
+    imagemDoAtor1 = loadImage("imagens/ator-1.png");
+    imagemCarro1 = loadImage("imagens/carro-1.png");
+    imagemCarro2 = loadImage("imagens/carro-2.png");
+    imagemCarro3 = loadImage("imagens/carro-3.png");
+    
+  }
